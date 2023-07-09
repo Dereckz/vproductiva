49311e8818fccab89614eb36077fdcd82b5c3503
Dereckz <dereckz1304@gmail.com>
2023-07-09 01:30:59 -0600

    lol

--- dev/functiones.php
+++ dev/functiones.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $database = "vproductiviabd";
 $username = "root";
-$password = "";
+$password = "x";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
