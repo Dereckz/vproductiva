88377ab3ae5fa6a667038e041f924e8daa30d7a9
Dereckz <dereckz1304@gmail.com>
2023-07-25 10:18:39 -0600

    guardar

--- panel/index.php
+++ panel/index.php
@@ -21,13 +21,13 @@
     <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon">
-          <img src="img/logo/LOGOVP.ico">
+          <img src="img/logo/logovproductiva2.png">
         </div>
-        <div class="sidebar-brand-text mx-3">Mestro</div>
+        <div class="sidebar-brand-text mx-3">Maestro</div>
       </a>
       <hr class="sidebar-divider my-0">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="index.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Incio</span></a>
       </li>
@@ -39,7 +39,7 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
           aria-expanded="true" aria-controls="collapseBootstrap">
           <i class="far fa-fw fa-window-maximize"></i>
-          <span>Bootstrap UI</span>
+          <span>Incio</span>
         </a>
         <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
@@ -67,7 +67,9 @@
           </div>
         </div>
       </li>
-      <li class="nav-item">
+
+      <!--tablqs example-->
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true"
           aria-controls="collapseTable">
           <i class="fas fa-fw fa-table"></i>
@@ -80,13 +82,14 @@
             <a class="collapse-item" href="datatables.html">DataTables</a>
           </div>
         </div>
-      </li>
-      <li class="nav-item">
+      </li> -->
+      <!-- colores ejemplo-->
+      <!-- <li class="nav-item">
         <a class="nav-link" href="ui-colors.html">
           <i class="fas fa-fw fa-palette"></i>
           <span>UI Colors</span>
         </a>
-      </li>
+      </li> -->
       <hr class="sidebar-divider">
       <div class="sidebar-heading">
         Examples
