d59de91a24d9a97e3eda99f2d433aaad401744cd
Dereckz <dereckz1304@gmail.com>
2023-09-07 10:03:17 -0600

    cambios de survey nuevos de este septiembre si

--- panel/func/agregarsurvey.php
+++ panel/func/agregarsurvey.php
@@ -7,28 +7,28 @@ $surveylist =array();
 $contadorlist=0;
 
     if ($btnSurvey) {
-        echo "Swal.fire(
-            'Operacion Exitosa',
-            '¡btn!',
-            'success'
-          )";
+       
         
-        $idusuario=$_SESSION["id"];
-            
+         $idusuario=$_SESSION["id"];
+     
+          
         $resultado = mysqli_query($conn, "SELECT * FROM survey_set ;");
             while ($consulta = mysqli_fetch_array($resultado)) {
             
                 $ids=$consulta["id"];
-
+             
                 if (isset($_REQUEST[$ids])) {
+                   // echo " Dato: ".$ids;
                     $surveylist[$ids]=$ids;
                 }
             }
+           
+              
             foreach ($surveylist as $val) {
                         // $queryA = mysqli_query($conn,"INSERT INTO `answers`(`id`, `survey_id`, `user_id`, `answer`, `question_id`, `date_created`) 
                         //  VALUES ('0','','$idusuario', ''.''.'')");
 
-                            $queryA = mysqli_query($conn,"INSERT INTO `detallesurvey`(`iIdDetalleSurvey `, `idSurvey`, `idUsuario`, `idAswer`) 
+                            $queryA = mysqli_query($conn,"INSERT INTO `detallesurvey`(`iIdDetalleSurvey`, `idSurvey`, `idUsuario`, `idAswer`) 
                             VALUES ('0','$val','$idusuario', '')");
                                 if($queryA){
                                     $contadorlist=$contadorlist+1;   
@@ -39,6 +39,7 @@ $contadorlist=0;
                         echo "<script>";
                         echo "MiFuncionJS();";
                         echo "</script>";
-                        header("Location: http://localhost/vproductivam/panel/instructor.php");
-        }
+                        header("Location: http://localhost/vproductivam/panel/instructor.php"); 
+                        /**/
+        } 
  ?>
