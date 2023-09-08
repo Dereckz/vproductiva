739105327efc7759f0acf65e52e283e5659d5877
Dereckz <dereckz1304@gmail.com>
2023-09-07 22:49:33 -0600

    survey instructor

---
+++ instructor/answer_survey.php
@@ -0,0 +1,507 @@
+<?php include ('consulta.php'); ?>
+<!DOCTYPE html>
+
+<!-- saved from url=(0091)https://website529400.nicepage.io/es/1234.html?version=56a80d83-e130-4c50-b76e-7bfc4c0ea516 -->
+<html style="font-size: 16px;" lang="en" class="u-responsive-xl"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="keywords" content="​Hello!">
+    <meta name="description" content="">
+    <title>Survey</title>
+    <link rel="stylesheet" href="./nicepage.css" media="screen" class="u-static-style">
+    <link rel="stylesheet" href="./nicepage(1).css" media="screen">
+    <script class="u-script" type="text/javascript" src="./jquery-1.9.1.min.js.descarga" defer=""></script>
+    <script class="u-script" type="text/javascript" src="./nicepage.js.descarga" defer=""></script>
+    <meta name="generator" content="Nicepage 4.18.8, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="./css">
+    <style class="u-style"> .u-section-2 {
+  background-image: none;
+}
+.u-section-2 .u-sheet-1 {
+  min-height: 50em;
+}
+.u-section-2 .u-text-1 {
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  --animation-custom_in-scale: 1;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: -300px;
+  /*margin: 90px 405px 0 0;*/
+  color: blue;
+  font-family: Open Sans;
+}
+.u-section-2 .u-text-2 {
+  font-size: 3rem;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: -300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  font-weight: 500;
+  /*margin: 42px 335px 0 0;*/
+  color: purple;
+}
+.u-section-2 .u-text-3 {
+  line-height: 2;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  /*margin: 20px 327px 0 0;*/
+}
+.u-section-2 .u-text-4 {
+  --animation-custom_in-scale: 1;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  margin: 35px 503px 0 0;
+}
+.u-section-2 .u-btn-1 {
+  margin-left: auto;
+  padding: 0;
+}
+.u-section-2 .u-list-1 {
+  margin-top: 52px;
+  margin-bottom: 0;
+}
+.u-section-2 .u-repeater-1 {
+  grid-template-columns: repeat(4, calc(25% - 22.5px));
+  min-height: 273px;
+  grid-gap: 30px;
+}
+.u-section-2 .u-list-item-1 {
+  background-image: url(img/carpetablanca.png);
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  /*box-shadow: 5px 5px 29px 0 rgba(0,0,0,0.15);*/
+  height: 15.4em;
+}
+.u-section-2 .u-container-layout-1 {
+  padding: 50px 30px 30px;
+}
+.u-section-2 .u-icon-1 {
+  height: 59px;
+  width: 59px;
+  background-image: none;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 0px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 180deg;
+  --animation-custom_in-scale: 1;
+  padding: 0;
+  margin-left: 4.5em;
+}
+.u-section-2 .u-text-5 {
+  font-size: 1.5rem;
+  font-weight: 400;
+  text-align: center;
+}
+.u-section-2 .u-btn-2 {
+  font-size: 0.875rem;
+  background-image: none;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 20px 0 0;
+  padding: 0;
+}
+.u-section-2 .u-list-item-2 {
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  box-shadow: 5px 5px 29px 0 rgba(0,0,0,0.15);
+}
+.u-section-2 .u-container-layout-2 {
+  padding: 50px 30px 30px;
+}
+.u-section-2 .u-icon-2 {
+  height: 59px;
+  width: 59px;
+  background-image: none;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 0px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 180deg;
+  --animation-custom_in-scale: 1;
+  margin: 0 auto 0 0;
+  padding: 0;
+}
+.u-section-2 .u-text-6 {
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 31px 0 0;
+}
+.u-section-2 .u-btn-3 {
+  font-size: 0.875rem;
+  background-image: none;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 20px 0 0;
+  padding: 0;
+}
+.u-section-2 .u-list-item-3 {
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  box-shadow: 5px 5px 29px 0 rgba(0,0,0,0.15);
+}
+.u-section-2 .u-container-layout-3 {
+  padding: 50px 30px 30px;
+}
+.u-section-2 .u-icon-3 {
+  height: 59px;
+  width: 59px;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 0px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 180deg;
+  --animation-custom_in-scale: 1;
+  margin: 0 auto 0 0;
+  padding: 0;
+}
+.u-section-2 .u-text-7 {
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 31px 0 0;
+}
+.u-section-2 .u-btn-4 {
+  font-size: 0.875rem;
+  background-image: none;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 20px 0 0;
+  padding: 0;
+}
+.u-section-2 .u-list-item-4 {
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+  box-shadow: 5px 5px 29px 0px rgba(0,0,0,0.15);
+}
+.u-section-2 .u-container-layout-4 {
+  padding: 50px 30px 30px;
+}
+.u-section-2 .u-icon-4 {
+  height: 59px;
+  width: 59px;
+  background-image: none;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 0px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 180deg;
+  --animation-custom_in-scale: 1;
+  margin: 0 auto 0 0;
+  padding: 0;
+}
+.u-section-2 .u-text-8 {
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 31px 0 0;
+}
+.u-section-2 .u-btn-5 {
+  font-size: 0.875rem;
+  background-image: none;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 20px 0 0;
+  padding: 0;
+}
+.u-section-2 .u-group-1 {
+  min-height: 257px;
+  margin-top: 52px;
+  margin-bottom: 60px;
+  height: auto;
+  --animation-custom_in-translate_x: 0px;
+  --animation-custom_in-translate_y: 300px;
+  --animation-custom_in-opacity: 0;
+  --animation-custom_in-rotate: 0deg;
+  --animation-custom_in-scale: 1;
+}
+.u-section-2 .u-container-layout-5 {
+  padding: 30px;
+}
+.u-section-2 .u-text-9 {
+  font-size: 3rem;
+  margin: 0 auto 0 0;
+}
+.u-section-2 .u-btn-6 {
+  border-style: solid;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 2px;
+  font-size: 0.875rem;
+  align-self: center;
+  text-align: center;
+  margin: 15px auto 0 0;
+  padding: 14px 46px 16px 44px;
+}
+@media (max-width: 1199px) {
+  .u-section-2 .u-text-1 {
+    margin-right: 205px;
+  }
+  .u-section-2 .u-text-2 {
+    margin-right: 135px;
+  }
+  .u-section-2 .u-text-3 {
+    margin-right: 127px;
+  }
+  .u-section-2 .u-text-4 {
+    margin-right: 303px;
+  }
+  .u-section-2 .u-repeater-1 {
+    min-height: 221px;
+  }
+  .u-section-2 .u-container-layout-1 {
+    padding-top: 40px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .u-section-2 .u-group-1 {
+    height: auto;
+  }
+}
+@media (max-width: 991px) {
+  .u-section-2 .u-sheet-1 {
+    min-height: 1439px;
+  }
+  .u-section-2 .u-text-1 {
+    margin-top: 60px;
+    margin-right: 0;
+  }
+  .u-section-2 .u-text-2 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-text-3 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-text-4 {
+    margin-right: 83px;
+  }
+  .u-section-2 .u-repeater-1 {
+    grid-template-columns: repeat(2, calc(50% - 15px));
+    min-height: 557px;
+  }
+  .u-section-2 .u-container-layout-1 {
+    padding-top: 30px;
+  }
+  .u-section-2 .u-container-layout-2 {
+    padding-top: 30px;
+  }
+  .u-section-2 .u-container-layout-3 {
+    padding-top: 30px;
+  }
+  .u-section-2 .u-container-layout-4 {
+    padding-top: 30px;
+  }
+  .u-section-2 .u-group-1 {
+    min-height: 302px;
+  }
+}
+@media (max-width: 767px) {
+  .u-section-2 .u-text-4 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-repeater-1 {
+    grid-auto-columns: calc(50% - 15px);
+  }
+  .u-section-2 .u-container-layout-2 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .u-section-2 .u-container-layout-3 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .u-section-2 .u-container-layout-4 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .u-section-2 .u-group-1 {
+    min-height: 302px;
+  }
+  .u-section-2 .u-container-layout-5 {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .u-section-2 .u-text-9 {
+    font-size: 2.25rem;
+  }
+  .u-section-2 .u-btn-6 {
+    margin-top: 20px;
+  }
+}
+@media (max-width: 575px) {
+  .u-section-2 .u-text-2 {
+    font-size: 2.25rem;
+  }
+  .u-section-2 .u-repeater-1 {
+    grid-template-columns: 100%;
+    grid-auto-columns: calc(100% - 0px);
+  }
+  .u-section-2 .u-container-layout-2 {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .u-section-2 .u-container-layout-3 {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .u-section-2 .u-container-layout-4 {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .u-section-2 .u-text-9 {
+    font-size: 1.875rem;
+  }
+}</style>
+    
+   <!--  <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "https://website529400.nicepage.io/1234.html"
+}</script> -->
+    <meta name="theme-color" content="#0066ff">
+    <meta property="og:title" content="1234">
+    <meta property="og:type" content="website">
+    <link rel="canonical" href="https://website529400.nicepage.io/1234.html">
+    <link rel="stylesheet" href="estiloencabezado.css">
+  
+</head>
+  <body class="u-body u-xl-mode" data-lang="en">
+  <header id="encabezado">
+    	<!--<div id="logotipo">
+    	<img src="img/logovproductiva2.png" id="logo"> 
+    	</div>-->
+    	<!--<h1 id="tituloprincipal">Valuacion Productiva</h1>-->
+    	<ul id="menu">
+      <li class="logo"><img src="img/logob.png" id="logo"></li>
+      <li class="menus"></li>
+    	<li class="menus"><a href="index.php">Mi perfil</a></li>
+    	<li class="menus"><a href="cursos.php">Catalogo de Cursos</a></li>
+      <li class="menus"><a href="../account/login.html">Salir</a></li>
+    	</ul>
+  </header>
+  <body>
+    <div>
+                  <?php include '../dev/conectar.php' ?>
+                  <?php 
+                        $qry = $conn->query("SELECT * FROM survey_set where id = ".$_GET['id'])->fetch_array();
+                        foreach($qry as $k => $v){
+                          if($k == 'title')
+                            $k = 'stitle';
+                          $$k = $v;
+                        }
+
+                  ?>
+                  <div class="col-lg-12">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="card card-outline card-primary">
+                          <div class="card-header">
+                            <h3 class="card-title"><b>Detalles de Encuesta</b></h3>
+                          </div>
+                          <div class="card-body p-0 py-2">
+                            <div class="container-fluid">
+                              <p>Titulo: <b><?php echo $stitle ?></b></p>
+                              <p class="mb-0">Descripción:</p>
+                              <small><?php echo $description; ?></small>
+                              <p>Inicio: <b><?php echo date("M d, Y",strtotime($start_date)) ?></b></p>
+                              <p>Fin: <b><?php echo date("M d, Y",strtotime($end_date)) ?></b></p>
+
+                            </div>
+                            <hr class="border-primary">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card card-outline card-success">
+                          <div class="card-header">
+                            <h3 class="card-title"><b>Cuestionario de encuesta</b></h3>
+                          </div>
+                          <form action="" id="manage-survey">
+                            <input type="hidden" name="survey_id" value="<?php echo $id ?>">
+                          <div class="card-body ui-sortable">
+                            <?php 
+                            $question = $conn->query("SELECT * FROM questions where survey_id = $id order by abs(order_by) asc,abs(id) asc");
+                            while($row=$question->fetch_assoc()):	
+                            ?>
+                            <div class="callout callout-info">
+                              <h5><?php echo $row['question'] ?></h5>	
+                              <div class="col-md-12">
+                              <input type="hidden" name="qid[<?php echo $row['id'] ?>]" value="<?php echo $row['id'] ?>">	
+                              <input type="hidden" name="type[<?php echo $row['id'] ?>]" value="<?php echo $row['type'] ?>">	
+                                <?php
+                                  if($row['type'] == 'radio_opt'):
+                                    foreach(json_decode($row['frm_option']) as $k => $v):
+                                ?>
+                                <div class="icheck-primary">
+                                              <input type="radio" id="option_<?php echo $k ?>" name="answer[<?php echo $row['id'] ?>]" value="<?php echo $k ?>" checked="">
+                                              <label for="option_<?php echo $k ?>"><?php echo $v ?></label>
+                                          </div>
+                                  <?php endforeach; ?>
+                              <?php elseif($row['type'] == 'check_opt'): 
+                                    foreach(json_decode($row['frm_option']) as $k => $v):
+                                ?>
+                                <div class="icheck-primary">
+                                              <input type="checkbox" id="option_<?php echo $k ?>" name="answer[<?php echo $row['id'] ?>][]" value="<?php echo $k ?>" >
+                                              <label for="option_<?php echo $k ?>"><?php echo $v ?></label>
+                                          </div>
+                                  <?php endforeach; ?>
+                              <?php else: ?>
+                                <div class="form-group">
+                                  <textarea name="answer[<?php echo $row['id'] ?>]" id="" cols="30" rows="4" class="form-control" placeholder="Write Something Here..." ></textarea>
+                                </div>
+                              <?php endif; ?>
+                              </div>	
+                            </div>
+                            <?php endwhile; ?>
+                          </div>
+                          </form>
+                          <div class="card-footer border-top border-success">
+                            <div class="d-flex w-100 justify-content-center">
+                              <button class="btn btn-sm btn-flat bg-gradient-primary mx-1" form="manage-survey" >Enviar respuesta</button>
+                              <button class="btn btn-sm btn-flat bg-gradient-secondary mx-1" type="button" onclick="location.href = 'survey.php'">Cancelar</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <script>
+
+                    $('#manage-survey').submit(function(e){
+                      e.preventDefault()
+                     // start_load()
+                      alert_toast("Thank You.",'success') 
+                     $.ajax({
+                        url:'http://localhost/vproductivam/panel/ajax.php?action=save_answer',
+                        method:'POST',
+                        data:$(this).serialize(),
+                        success:function(resp){s
+                          if(resp == 1){
+                            alert_toast("Thank You.",'success')
+                            setTimeout(function(){
+                              location.href = 'http://localhost/vproductivam/instructor/'
+                            },2000)
+                          }
+                        }
+                      }) 
+                    })
+                  </script>
+          </div>
+  </body>
+</html>
