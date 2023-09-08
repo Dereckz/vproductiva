3a60580c6ebc95366e4f7bf8b5cc607df915b040
Dereckz <dereckz1304@gmail.com>
2023-09-08 00:21:52 -0600

    Survey tester qwestion p2

--- instructor/survey.php
+++ instructor/survey.php
@@ -418,8 +418,7 @@
                       <div class="col-md-3 py-1 px-1 survey-item">
                         <div class="card card-outline card-primary">
                                 <div class="card-header">
-                                  <h3 class="card-title" style=""><?php ucwords($row['title']) ?></h3>
-
+                                <h3 class="card-title"><?php echo ucwords($row['title']) ?></h3>
                                   <div class="card-tools">
                                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                       <i class="fas fa-minus">-</i>
