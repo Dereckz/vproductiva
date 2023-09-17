2ef64ecccbdcc9a1f3c93bd1dc82ecdd05ef500b
Dereckz <dereckz1304@gmail.com>
2023-09-17 02:36:33 -0600

    merge meriinosx

--- panel/instructor.php
+++ panel/instructor.php
@@ -70,7 +70,6 @@
  
     <?php include 'func/cursos.php';?>
     <?php include 'func/surveyset.php';?>
-
     <?php
     include( "..\dev\conectar.php");
 
@@ -120,19 +119,22 @@
                               <td><span  class="badge badge-danger"><a class="text-white" href="func/actualizarStatus.php?id='<?php echo $dataCliente['iIdUsuario']; ?>'&status=0">Inactivo</a></span></td> 
                             <?php }?> 
                             <td>
-                              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteChildresn<?php echo $dataCliente['iIdUsuario']; ?>">
+<!--                          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteChildresn<?php echo $dataCliente['iIdUsuario']; ?>">
                                 Eliminar
                               </button>
-
+ -->
                               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editChildresn<?php echo $dataCliente['iIdUsuario']; ?>">
                                 Modificar
                               </button>
-                              <button type="button" class=" btn btn-warning " data-toggle="modal" data-target="#editChildresn<?php echo $dataCliente['iIdUsuario']; ?>">
+                              <button type="button" class="btn btn-warning" data-toggle="modal"  data-target="#cursomodal<?php echo $dataCliente['iIdUsuario']; ?>">
                                 Agregar Curso
                               </button>
-                              <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editChildresn<?php echo $dataCliente['iIdUsuario']; ?>">
+                              <button type="button" class="btn btn-success" data-toggle="modal" data-target="#surveymodal<?php echo $dataCliente['iIdUsuario']; ?>">
                                 Agregar Encuesta
                               </button>
+                            </td>
+                            <td>
+                              <?php echo''.cursodeusuario($dataCliente['iIdUsuario'])?>
                             </td>
                           </tr>
 
@@ -143,6 +145,11 @@
                           <!--Ventana Modal para la Alerta de Eliminar--->
                           <?php include('ModalEliminar.php'); ?>
 
+                          <!--Ventana Modal para Agregar Curso--->
+                          <?php include('ModalCurso.php'); ?>
+
+                          <!--Ventana Modal para Agregar Encuesta--->
+                          <?php include('ModalEncuesta.php'); ?>
 
                         <?php } ?>
 
@@ -153,10 +160,8 @@
                 </div>
               </div>
             </div>
-           
-            </div>
+          </div>
        
-
           <!-- Modal Logout -->
           <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
                   aria-hidden="true">
@@ -177,119 +182,8 @@
                       </div>
                     </div>
                   </div>
-                </div> 
-
-          <!--modal agregar curso-->
-         
-          <div class="modal fade" id="cursomodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabelLogout">Asignar Curso</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-            <form name="frmAgregar" method="post" action="http://localhost/vproductivam/panel/func/agregarcurso.php">  
-                <div class="modal-body">
-                <label for="curso-names">Seleccione curso a Asignar:</label> 
-                <select name="cursoselect" id="cursoselect">
-                      <!--?php listcurso()?--> 
-                      <option value='1'>Productividad Laboral</option>
-                      <option value='2'>Habilidades Blandas</option>
-                      <option value='3'>Habilidades Digitales</option>
-                      <option value='4'>Psicología</option>
-                      <option value='5'>Salud, Higiene y Seguridad</option>
-                      <option value='6'>Cultura Jurídica / derecho empresarial y corporativo</option>
-                      <option value='7'>Finanzas</option>
-                  </select>
-                </div>
-                <div class="modal-footer">
-                  <button type="submit" class="btn btn-outline-primary"  id ="btnAsignar" name ="btnAsignar"  >Agregar</button>
-                  <a  class="btn btn-outline-primary" data-dismiss="modal">Salir</a>
-                </div>
-            </form>    
-              </div>
-            </div>
-          </div>    
-        </div>
-
-        <!--Modal añadir Encuesta -->
-        <div class="modal fade" id="surveymodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabelLogout">Mandar Encuesta</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                    <form name="frmSurveySet" method="post" action="func/agregarsurvey.php">  
-                        <div class="modal-body">
-                        <label for="curso-names">Marque encuesta(s):</label> 
-                       <div>
-                        
-
-                         <?php showAllSurvey() ?> 
-                       </div>  
-                        </div>
-                        <div class="modal-footer">
-                          <button type="submit" class="btn btn-outline-primary"  id ="btnSurvey" name ="btnSurvey"  >Agregar</button>
-                          <a  class="btn btn-outline-primary" data-dismiss="modal">Salir</a>
-                        </div>
-                    </form>    
-              </div>
-            </div>
-          </div>
-          
-                <!--Modal editar trabajador -->
-        <div class="modal fade" id="usermodal" tabindex="-1" role="dialog"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabelLogout">Editar Usuario</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <form action="" id="manage-user">	
-                <div class="modal-body">
-                          <input type="hidden" name="id" value="<?php echo isset($meta['id']) ? $meta['id']: '' ?>">
-                          <div class="form-group">
-                            <label for="name">First Name</label>
-                            <input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo isset($meta['firstname']) ? $meta['firstname']: '' ?>" required>
-                          </div>
-                          <div class="form-group">
-                            <label for="name">Middle Name</label>
-                            <input type="text" name="middlename" id="middlename" class="form-control" value="<?php echo isset($meta['middlename']) ? $meta['middlename']: '' ?>">
-                          </div>
-                          <div class="form-group">
-                            <label for="name">Last Name</label>
-                            <input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo isset($meta['lastname']) ? $meta['lastname']: '' ?>" required>
-                          </div>
-                          <div class="form-group">
-                            <label for="username">Email</label>
-                            <input type="text" name="email" id="email" class="form-control" value="<?php echo isset($meta['email']) ? $meta['email']: '' ?>" required  autocomplete="off">
-                          </div>
-                          <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
-                            <small><i>Leave this blank if you dont want to change the password.</i></small>
-                          </div>
-                      <div>
-
-                      <div class="modal-footer">
-                          <button type="submit" class="btn btn-outline-primary"  id ="btnSurvey" name ="btnSurvey"  >Agregar</button>
-                          <a  class="btn btn-outline-primary" data-dismiss="modal">Salir</a>
-                        </div>
-	              </form>  
-              </div>
-            </div>
-          </div>   
-
+          </div> 
+                            
         </div>
          
     <!-- /.content -->
