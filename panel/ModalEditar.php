eaf7847859652cdfdbdc2bee454de3ea9ee9878e
Dereckz <dereckz1304@gmail.com>
2023-09-17 01:28:03 -0600

    modificaciones curso, encuesta, editar

---
+++ panel/ModalEditar.php
@@ -0,0 +1,84 @@
+
+<!--ventana para Update--->
+<div class="modal fade" id="editChildresn<?php echo $dataCliente['iIdUsuario']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color: #563d7c !important;">
+        <h6 class="modal-title" style="color: #fff; text-align: center;">
+            Actualizar Información
+        </h6>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+
+      <form method="POST" action="func/actualizaruser.php">
+        <input type="hidden" name="idusuario" value="<?php echo $dataCliente['iIdUsuario']; ?>">
+
+            <div class="modal-body" id="cont_modal">
+
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Nombre(s):</label>
+                  <input type="text" name="nombre" class="form-control" value="<?php echo $dataCliente['cNombre']; ?>" required="true">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Apellido Paterno:</label>
+                  <input type="text" name="app" class="form-control" value="<?php echo $dataCliente['cApellidoP']; ?>" required="true">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Apellido Materno:</label>
+                  <input type="text" name="apm" class="form-control" value="<?php echo $dataCliente['cApellidoM']; ?>" required="true">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Correo:</label>
+                  <input type="email" name="email" class="form-control" value="<?php echo $dataCliente['cCorreo']; ?>" required="true">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Password:</label>
+                  <input type="password" name="password" class="form-control" value="" >
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Telefono:</label>
+                  <input type="number" name="celular" class="form-control" value="<?php echo $dataCliente['cTelefono']; ?>" >
+                </div>
+                <div>
+                  <?php 
+                    
+                    if ($dataCliente["iGenero"]==0){
+                      echo ' 
+                      <input type="radio" name="radio" value="0" checked=true> Masculino<br>
+                      <input type="radio" name="radio" value="1"> Femenino<br>
+                      <input type="radio" name="radio" value="2"> No especificar<br>';
+                    }elseif($dataCliente["iGenero"]==1){
+                        echo ' 
+                        <input type="radio" name="radio" value="0" > Masculino<br>
+                        <input type="radio" name="radio" value="1"checked=true> Femenino<br>
+                        <input type="radio" name="radio" value="2"> No especificar<br>';
+                      }else{
+                        echo ' 
+                        <input type="radio" name="radio" value="0" > Masculino<br>
+                        <input type="radio" name="radio" value="1"> Femenino<br>
+                        <input type="radio" name="radio" value="2" checked=true> No especificar<br>';
+                      }
+                    
+                    ?>
+                    </div>
+                    <div class="form-group">
+                      <label class="col-form-label" for="">Fecha de alta</label>
+                          <?php
+                          echo '  <input type="date" name="fechaalta" class="form-control" value="'.$dataCliente["dFechaAlta"].'">';
+                        ?>
+                    </div>
+                
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+              <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+            </div>
+       </form>
+
+    </div>
+  </div>
+</div>
+<!---fin ventana Update --->
--- panel/func/actualizaruser.php
+++ panel/func/actualizaruser.php
@@ -1,23 +1,40 @@
 <?php
 include("..\..\dev\conectar.php");
 $idusuario=$_POST["idusuario"];
-$email=$_POST["email"];
-$pass=password_hash(($_POST["password"]), PASSWORD_DEFAULT);   
+$email=$_POST["email"];  
 $nombre=$_POST["nombre"];
 $apellidoparterno=$_POST["app"];
 $apellidomaterno=$_POST["apm"];
 $genero=$_POST["radio"];
+$telefono=$_POST["celular"];
 $fechaalta=$_POST["fechaalta"];
 
-$query = mysqli_query($conn,"UPDATE Usuarios
-                            SET cNombre='".$nombre."'".
-                            ", cApellidoP='".$apellidoparterno."'".
-                            ", cApellidoM='".$apellidomaterno."'".
-                            ", cCorreo='".$email."'".
-                            ", iGenero='".$genero."'".
-                            ", dFechaAlta='".$fechaalta."'" .
-                            ", cPassword='".$pass."'" .
-                        " where iIdUsuario=".$idusuario);
+if (($_POST["password"])==""){
+    $consulta="UPDATE Usuarios
+        SET cNombre='".$nombre."'".
+        ", cApellidoP='".$apellidoparterno."'".
+        ", cApellidoM='".$apellidomaterno."'".
+        ", cCorreo='".$email."'".
+        ", iGenero='".$genero."'".
+        ", cTelefono='".$telefono."'" .
+        ", dFechaAlta='".$fechaalta."'" .
+        " where iIdUsuario=".$idusuario;
+}elseif(($_POST["password"])<>""){
+        $pass=password_hash(($_POST["password"]), PASSWORD_DEFAULT); 
+        $consulta="UPDATE Usuarios
+                SET cNombre='".$nombre."'".
+                ", cApellidoP='".$apellidoparterno."'".
+                ", cApellidoM='".$apellidomaterno."'".
+                ", cCorreo='".$email."'".
+                ", iGenero='".$genero."'".
+                ", dFechaAlta='".$fechaalta."'" .
+                ", cTelefono='".$telefono."'" .
+                ", cPassword='".$pass."'" .
+            " where iIdUsuario=".$idusuario;
+}
+    $query = mysqli_query($conn,$consulta);
+
+
             if($query){
             echo '<script src="sweetalert2.all.min.js"></script>';
             echo'  <link href="sweetalert2.min.css" rel="stylesheet">';
@@ -27,7 +44,7 @@ $query = mysqli_query($conn,"UPDATE Usuarios
                 'Actualizado Correctamente.',
                 'success'";
             echo "</script>";
-            header("Location: http://localhost/vproductivam/panel/instructor_edit.php?idu=".$idusuario);
+            header("Location: http://localhost/vproductivam/panel/instructor.php");
 
             }
 
