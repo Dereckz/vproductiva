f40f3febb50e034e8e87f4437a3664f757f91042
Dereckz <dereckz1304@gmail.com>
2023-09-18 18:16:11 -0600

    survey aswers

--- instructor/respuestas.php
+++ instructor/respuestas.php
@@ -1,22 +1,22 @@
 
-<script>
-echo "esto es qid".$get['qid']
-$('#manage-survey').submit(function(e){
-  e.preventDefault()
- // start_load()
-  alert_toast("Thank You.",'success') 
- $.ajax({
-    url:'http://localhost/vproductivam/panel/ajax.php?action=save_answer',
-    method:'POST',
-    data:$(this).serialize(),
-    success:function(resp){s
-      if(resp == 1){
-        alert_toast("Thank You.",'success')
-        setTimeout(function(){
-          location.href = 'http://localhost/vproductivam/instructor/'
-        },2000)
-      }
-    }
-  }) 
-})
-</script>
+<?php
+ include "..\dev\conectar.php";
+
+$idencuesta=$_GET["survey_id"];
+$idrespuesta=$_GET["qid"];
+
+foreach($qid as $k => $v){
+  $data = " survey_id=$survey_id ";
+				$data .= ", question_id='$qid[$k]' ";
+				$data .= ", user_id='{$_SESSION['login_id']}' ";
+				if($type[$k] == 'check_opt'){
+					$data .= ", answer='[".implode("],[",$answer[$k])."]' ";
+				}else{
+					$data .= ", answer='$answer[$k]' ";
+				}
+			//	$save[] = $this->db->query("INSERT INTO answers set $data");
+}
+echo 'Survey Id='.$idencuesta;
+echo 'Aswerd Id='.$idrespuesta;
+
+?>
