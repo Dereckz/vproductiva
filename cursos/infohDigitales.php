d84a47d0a9617589f703050de4b0b2e5388bf6c4
Cesar Merino <cesar.merinopmc@gmail.com>
2023-09-25 23:52:08 -0600

    cambios merino parte 3 25092023

--- cursos/infohDigitales.php
+++ cursos/infohDigitales.php
@@ -30,7 +30,7 @@
   
 
 <?php
- include("..\dev\conectar.php");
+ require("../dev/conectar.php");
  $resultado = mysqli_query($conn,"SELECT c.cNombreCurso, m.cNombreModulo FROM curso c
  INNER JOIN modulo m ON c.iIdCurso = m.fkiIdCurso WHERE c.iIdCurso=3");
 while ($consulta = mysqli_fetch_array($resultado))
