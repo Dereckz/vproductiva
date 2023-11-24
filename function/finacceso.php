3e139b8b20de2a3f914da273137af0babb07d0f8
Dereckz <dereckz1304@gmail.com>
2023-11-24 00:24:17 -0600

    chingon mis cambios

--- function/finacceso.php
+++ function/finacceso.php
@@ -6,7 +6,8 @@
  $fecha = date("Y-m-d");
  $fechaactual = date("Y-m-d H:i:s");
  $horaActual = date("h:i:s");
- //hacer consulta, con el id y la fecha de acces, para obtener informacion relacionada
+ $idCliente="";
+  //hacer consulta, con el id y la fecha de acces, para obtener informacion relacionada
     $sqlCliente   = ("SELECT MAX(idAccesos) as id 
                     FROM accesos WHERE idusuario=".$_SESSION["id"]."
                      and dfechaacceso like '%" . $fecha ."%';");
@@ -15,12 +16,20 @@
     $cantidad     = mysqli_num_rows($queryCliente);
     while ($dataCliente = mysqli_fetch_array($queryCliente)) { 
         $idCliente = $dataCliente["id"];
-
+        $queryA = mysqli_query($conn,"UPDATE accesos SET dFechaCierre ='".$fechaactual."' 
+        WHERE idusuario=".$_SESSION["id"]."
+        and dfechaacceso = SELECT MAX(dfechaacceso)
+        FROM accesos;");
     }
  
- $queryA = mysqli_query($conn,"UPDATE accesos SET dFechaCierre='".$fechaactual."';");
+  
      
      if($queryA){
+       //Removemos sesión.
+       session_unset();
+       //Destruimos sesión.
+       session_destroy();              
+       //Redirigimos pagina.
         header("Location:  ../index.html");
      }
 
