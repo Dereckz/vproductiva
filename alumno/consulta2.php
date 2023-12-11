a37c0e7a3beaa28a0ad0f703983fd3c36ff1457b
Merino <cesar.merinopmc@gmail.com>
2023-12-11 15:28:35 -0600

    cambios Merino 11122023

--- alumno/consulta2.php
+++ alumno/consulta2.php
@@ -7,7 +7,7 @@ function infoCurso2($idarea )
     
     require("../dev/conectar.php");
     
-        $inscrito = mysqli_query($conn, "SELECT c.iIdCurso 
+    $inscrito = mysqli_query($conn, "SELECT c.iIdCurso 
         FROM curso c
         INNER JOIN inscripcion i ON i.fkiIdeCurso = c.iIdCurso
         INNER JOIN usuarios u ON u.iIdUsuario = i.fkiIdUsuario
@@ -210,6 +210,43 @@ function infoCurso2($idarea )
    }
    
  
+}
+
+function incribirse($idcurso){
+    
+    require("../../dev/conectar.php");
+    $inscrito = mysqli_query($conn, "SELECT c.iIdCurso 
+    FROM curso c
+    INNER JOIN inscripcion i ON i.fkiIdeCurso = c.iIdCurso
+    INNER JOIN usuarios u ON u.iIdUsuario = i.fkiIdUsuario
+    WHERE i.finalizado !=2 AND i.fkiIdUsuario=" . $_SESSION["id"]);
+
+
+$arreglo= array();
+while ($infoinscrito = mysqli_fetch_array($inscrito)) {
+
+array_push($arreglo, $infoinscrito['iIdCurso']);
+
+
+}
+
+
+       //validar si esta inscrito en el curso
+       $link ='<div style="background-color: #00b0ff; border-radius: 25em; color: white; padding: 0em 1em; margin: 0em 1em;">
+       <a href="inscripcion.php? curso='.$idcurso.'&usuario='.$_SESSION["id"].'" class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">Inscribirse</a></div>';
+       $i=0;
+       while($i<count($arreglo)){
+           if ($arreglo[$i]== $idcurso){
+               $link ='<div style="background-color: #00b0ff; border-radius: 25em; color: white; padding: 0em 1em; margin: 0em 1em;">
+               <a href="#" id="" class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">Inscrito</a></div>';  
+               $i=count($arreglo);
+           }
+           else{
+           $link ='<div style="background-color: #00b0ff; border-radius: 25em; color: white; padding: 0em 1em; margin: 0em 1em;">
+           <a href="inscripcion.php?curso='. $idcurso .'&usuario='.$_SESSION["id"].'"  class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">Inscribirse</a></div>';
+           } 
+           $i++;       
+       }
 }
 
 
--- cursos/funct/functiones.php
+++ cursos/funct/functiones.php
@@ -16,25 +16,23 @@ WHERE i.finalizado !=2 AND i.fkiIdUsuario=" . $_SESSION["id"]);
     
     } 
     $link ='
-    <div style="background-color: #00b0ff; 
-    border-radius: 25em; color: white; 
-    padding: 0em 1em; 
-    margin: 0em 1em;">
+    <div class="btnInscribirse">
         <a href="../../alumno/inscripcion.php?curso='.$idcurso.'&usuario='.$_SESSION["id"].'" 
+        class="btnInscribirsebtn"
         class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">
-        Inscribirse</a>
+        INSCRIBIRSE</a>
     </div>';
     $i=0;
     while($i<count($arreglo)){
 
         if ($arreglo[$i]== $idcurso){
-            $link ='<div style="background-color: #00b0ff; border-radius: 25em; color: white; padding: 0em 1em; margin: 0em 1em;">
-            <a href="#" id="" class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">Inscrito</a></div>';  
+            $link ='<div class="btnInscribirse">
+            <a href="#" id="" class="btnInscribirsebtn" class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">INSCRITO</a></div>';  
             $i=count($arreglo);
         }
         else{
-        $link ='<div style="background-color: #00b0ff; border-radius: 25em; color: white; padding: 0em 1em; margin: 0em 1em;">
-        <a href="../../alumno/inscripcion.php?curso='.$idcurso.'&usuario='.$_SESSION["id"].'"  class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">Inscribirse</a></div>';
+        $link ='<div class="btnInscribirse" class="btnInscribirsebtn">
+        <a href="../../alumno/inscripcion.php?curso='.$idcurso.'&usuario='.$_SESSION["id"].'"  class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">INSCRIBIRSE</a></div>';
         } 
         $i++;       
     }
