0e64145790a8f8161426ac72b1f6cfcf85fb0428
Dereckz <dereckz1304@gmail.com>
2024-01-09 00:03:02 -0600

    reconocmineto

--- reward/reconocimiento.php
+++ reward/reconocimiento.php
@@ -5,13 +5,14 @@ require("../dev/conectar.php");
 
 date_default_timezone_set('America/Mexico_City');
 
+header("Content-Type: text/html; charset=iso-8859-1 ");
+
 if (!isset($_SESSION)) {
     session_start();
 }
-//$fecha = date("d-m-y");
 
-//$curso =$_GET['curso'];
 $idCur =$_GET['idCurso'];
+
 
 $consCurs = mysqli_query($conn, "SELECT * FROM  inscripcion WHERE fkiIdeCurso =".$idCur." and fkiIdUsuario =".$_SESSION["id"]);
 $fecha = mysqli_fetch_array($consCurs);
@@ -39,10 +40,13 @@ $pdf->SetY(105);
 $pdf->SetX(105);
 $pdf->Cell(160,15, mb_convert_encoding( $consulta['cNombre'].' '.$consulta['cApellidoP'].' '.$consulta['cApellidoM'], 'UTF-8', mb_list_encodings()),0,0,'C');
 
+
+    //$infocurso["cNombreCurso"]
+ 
 $pdf->SetFont('Times','I',20);
 $pdf->SetY(135);
 $pdf->SetX(100);
-$pdf->Cell(160,15,iconv('ISO-8859-1', 'UTF-8', $infocurso["cNombreCurso"]),0,0,'C');
+$pdf->Cell(160,15,html_entity_decode($infocurso["cNombreCurso"]),0,0,'C');
 
 
 $pdf->SetFont('Times','I',15);
