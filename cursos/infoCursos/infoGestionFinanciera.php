57d0584c3d6c68495bf0f6c209a5ff8280a7b1eb
Merino <cesar.merinopmc@gmail.com>
2024-01-24 12:27:13 -0600

    cambios Merino 24012024

--- cursos/infoCursos/infoGestionFinanciera.php
+++ cursos/infoCursos/infoGestionFinanciera.php
@@ -1,3 +1,6 @@
+
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,38 +19,62 @@
             <li class="logo"><img src="../img/logob.png" id="logo"></li>
             <li class="menus"></li>
     	    <li class="menus"><a href="../../alumno/index.php" class="enlacemenu">Mi perfil</a></li>
-    	    <li class="menus"><a href="../../alumno/cursos.php" class="enlacemenu">Catálogo de Cursos</a></li>
+    	    <li id="miperfilfondo" class="menus"><a href="../../alumno/cursos.php" id="miperfil" class="enlacemenu">Catálogo de Cursos</a></li>
             <li class="menus"><a href="../../account/login.html" class="enlacemenu">Salir</a></li>
     	</ul>
     </header>
 
     <div id="fondotemario">
     
-    <img class="tituloscursos" src="../img/NameCurso.png" >
+    <!--<img class="tituloscursos" src="../img/etica.png" >-->
+    <h1 id="tituloprincipal">GESTIÓN FINANCIERA</h1>
     <table  id=tablarecursos>
         <tr>
-            <td><img src="img/etica.jpg" ></td>
+            <td id="imagentemario"><img src="img/financiera.png" id="imagentemar"></td>
             <td class="celdatop">
             <table id=tablacontenido>
-                <tr> <td id=filatitulo colspan="2"> <h2>Contenido</h2> </td></tr>
+                <tr> <td></td><td> <h3 id="descricpiontemario">
+                ¿Deseas tomar el control de tus finanzas personales o mejorar la salud financiera de tu empresa? Nuestro Curso de Gestión Financiera es la clave para alcanzar el éxito financiero y económico que deseas. Este curso está diseñado para proporcionarte las habilidades y el conocimiento necesarios para tomar decisiones financieras estratégicas y eficaces.
+                </br>"Gana ventaja competitiva con una gestión financiera efectiva."
+                </h3> </td></tr>
+                <tr> <td></td><td id=filatitulo> <h2>TEMARIO</h2> </td></tr>
                 
-                <!--<tr> <td id=filainstrucciones> <h4> En este curso aprenderas las habilidades necesarias para optimizar tus actividades diarias.</h4> </td></tr>  -->  
-    
+                <!--<tr> <td id=filainstrucciones> <h4> En este curso aprenderas las habilidades necesarias para optimizar tus actividades diarias.</h4> </td></tr>  -->
                 <?php
                  require("../../dev/conectar.php");
                 $resultado = mysqli_query($conn,"SELECT c.cNombreCurso, m.cNombreModulo FROM curso c
                 INNER JOIN modulo m ON c.iIdCurso = m.fkiIdCurso WHERE c.iIdCurso=23");
+                $modulo=0;
                 while ($consulta = mysqli_fetch_array($resultado))
                 {
-                    echo '<tr> <td class=sangriavinetas><img src="img/vineta.png" style= "width: 0.5em;"></td> <td class=titulostemario2>'.$consulta['cNombreModulo'] .'</td></tr>
-                    <tr><td></td><td class=celdasvacias2></td></tr>';
-
+                    $modulo=$modulo+1;
+                    echo '<tr> 
+                            <td class=sangriavinetas></td>
+                             <td class=titulostemario2><span id="nmodulo">Módulo '.$modulo. '.</span> </br> '.$consulta['cNombreModulo'] .'</td>
+                          </tr>
+                         <tr>
+                            <td></td>
+                            <td class=celdasvacias2>
+                            </td>
+                         </tr>';
                 }
-                ?>
                 
+                ?>  
+                <tr> 
+                    <td>
+                    </td>
+                    <td >
+                    <?php include('..\funct\functiones.php'); 
+                    ?>
+                   
+                    </td>
+                </tr>       
+                
+           
             </table> 
             </td>
         </tr>
     </table>
+                
     </body>
 </html> 
--- cursos/infoCursos/infoProyeccion.php
+++ cursos/infoCursos/infoProyeccion.php
@@ -1,10 +1,13 @@
+
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="estiloencabezado.css">
         
-        <title>Proyeccion Financiera</title>
+        <title>Proyección financiera</title>
     </head>
     <body>
     <header id="encabezado">
@@ -16,38 +19,62 @@
             <li class="logo"><img src="../img/logob.png" id="logo"></li>
             <li class="menus"></li>
     	    <li class="menus"><a href="../../alumno/index.php" class="enlacemenu">Mi perfil</a></li>
-    	    <li class="menus"><a href="../../alumno/cursos.php" class="enlacemenu">Catálogo de Cursos</a></li>
+    	    <li id="miperfilfondo" class="menus"><a href="../../alumno/cursos.php" id="miperfil" class="enlacemenu">Catálogo de Cursos</a></li>
             <li class="menus"><a href="../../account/login.html" class="enlacemenu">Salir</a></li>
     	</ul>
     </header>
 
     <div id="fondotemario">
     
-    <img class="tituloscursos" src="../img/NameCurso.png" >
+    <!--<img class="tituloscursos" src="../img/etica.png" >-->
+    <h1 id="tituloprincipal">PROYECCIÓN FINANCIERA</h1>
     <table  id=tablarecursos>
         <tr>
-            <td><img src="img/etica.jpg" ></td>
+            <td id="imagentemario"><img src="img/proyeccion.png" id="imagentemar"></td>
             <td class="celdatop">
             <table id=tablacontenido>
-                <tr> <td id=filatitulo colspan="2"> <h2>Contenido</h2> </td></tr>
+                <tr> <td></td><td> <h3 id="descricpiontemario">
+                La proyección financiera es una herramienta esencial para tomar decisiones estratégicas, tanto a nivel personal como empresarial. Nuestro Curso de Proyección Financiera, está diseñado para equiparte con habilidades necesarias para planificar y gestionar tus finanzas de manera efectiva, tomar decisiones informadas sobre inversiones, gastos y crecimiento. Aprenderás a utilizar datos financieros para anticipar el futuro y ejecutar decisiones sólidas.
+                </br>"Aprende a anticipar oportunidades y desafíos financieros con herramientas de proyección"
+                </h3> </td></tr>
+                <tr> <td></td><td id=filatitulo> <h2>TEMARIO</h2> </td></tr>
                 
-                <!--<tr> <td id=filainstrucciones> <h4> En este curso aprenderas las habilidades necesarias para optimizar tus actividades diarias.</h4> </td></tr>  -->  
-    
+                <!--<tr> <td id=filainstrucciones> <h4> En este curso aprenderas las habilidades necesarias para optimizar tus actividades diarias.</h4> </td></tr>  -->
                 <?php
                  require("../../dev/conectar.php");
                 $resultado = mysqli_query($conn,"SELECT c.cNombreCurso, m.cNombreModulo FROM curso c
                 INNER JOIN modulo m ON c.iIdCurso = m.fkiIdCurso WHERE c.iIdCurso=24");
+                $modulo=0;
                 while ($consulta = mysqli_fetch_array($resultado))
                 {
-                    echo '<tr> <td class=sangriavinetas><img src="img/vineta.png" style= "width: 0.5em;"></td> <td class=titulostemario2>'.$consulta['cNombreModulo'] .'</td></tr>
-                    <tr><td></td><td class=celdasvacias2></td></tr>';
-
+                    $modulo=$modulo+1;
+                    echo '<tr> 
+                            <td class=sangriavinetas></td>
+                             <td class=titulostemario2><span id="nmodulo">Módulo '.$modulo. '.</span> </br> '.$consulta['cNombreModulo'] .'</td>
+                          </tr>
+                         <tr>
+                            <td></td>
+                            <td class=celdasvacias2>
+                            </td>
+                         </tr>';
                 }
-                ?>
                 
+                ?>  
+                <tr> 
+                    <td>
+                    </td>
+                    <td >
+                    <?php include('..\funct\functiones.php'); 
+                    ?>
+                   
+                    </td>
+                </tr>       
+                
+           
             </table> 
             </td>
         </tr>
     </table>
+                
     </body>
 </html> 
