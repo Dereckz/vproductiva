6e1ae41df41dcdaffe75410e4c314692261b2cf0
Dereckz <dereckz1304@gmail.com>
2024-01-26 12:33:31 -0600

    soolle

--- cursos/infoCursos/issue.php
+++ cursos/infoCursos/issue.php
@@ -47,7 +47,7 @@
                 <?php
                 
                 $resultado = mysqli_query($conn,"SELECT c.cNombreCurso, m.cNombreModulo FROM curso c
-                INNER JOIN modulo m ON c.iIdCurso = m.fkiIdCurso WHERE c.iIdCurso=1");
+                INNER JOIN modulo m ON c.iIdCurso = m.fkiIdCurso WHERE c.iIdCurso=".$idc);
                 $modulo=0;
                 while ($consulta = mysqli_fetch_array($resultado))
                 {
