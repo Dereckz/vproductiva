7d55c011c953bebf930cc581c2cc02131213deae
Dereckz <dereckz1304@gmail.com>
2024-01-25 19:19:34 -0600

    soy una verga

--- cursos/detailcurso.php
+++ cursos/detailcurso.php
@@ -83,7 +83,7 @@ $modulo=$modulo+1;
                 <td class=iconorecursocelda> '.$visto.'</td> 
                 <td class=fondotemario>
                     <a class=titulostemario  href="'.$consulta['cRuta'].'" id="'.$consulta['iIdRecurso'].'" target="_blank" class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">
-                    Modulo '.$modulo.': '.$consulta['cNombreModulo'].'
+                    Modulo '.$modulo.':<br> '.$consulta['cNombreModulo'].'
                     </a></td> 
                 </tr>
              <tr>
