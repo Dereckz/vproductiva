d5fb65830c2c8f0031e2128ee6a6fa27c7756b5d
Merino <cesar.merinopmc@gmail.com>
2024-02-01 11:19:30 -0600

    cambios Merino 30012024

--- cursos/detailcurso.php
+++ cursos/detailcurso.php
@@ -157,7 +157,7 @@
                         <a href="../cursos/evaluacion.php?idC='.$idcursoseleccionado.'" 
                         id="texconstancia" target="_blank" 
                         class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">
-                        Realizar Examens
+                        Realizar Examen
                         <br>
                         </a>
                     </td>
