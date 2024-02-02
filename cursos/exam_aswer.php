22f88351fca387ad4d6b2ea20bc7f2dfb7b57919
Dereckz <dereckz1304@gmail.com>
2024-02-02 02:28:04 -0600

    revsa

--- cursos/exam_aswer.php
+++ cursos/exam_aswer.php
@@ -18,13 +18,13 @@ $fecha= date("Y-m-d H:i:s");
 $intento="1";
 $iestatus="";
 
-        $dataresultado = mysqli_query($conn, "SELECT * FROM resuelto 
+    $dataresultado = mysqli_query($conn, "SELECT * FROM resuelto 
                                         WHERE idcurso=" .$idcurso.
                                         " AND idusuario=" .$idusuario.
-                                        " AND fecha like '%".date("Y-m")."%' order by fecha DESC  LIMIT 1;");
+                                         " AND fecha like '%".date("Y-m")."%' order by fecha DESC LIMIT 1;");
 
     $inforesultado = mysqli_fetch_array($dataresultado);
-
+    $rowresultado=mysqli_num_rows($dataresultado);
     $data="";
     
         foreach($idpregunta as $pid){
@@ -59,49 +59,55 @@ $iestatus="";
                
              endwhile;
                 //primer intento
-             if($inforesultado["intento"]<=0){
+                if($rowresultado>0){
+                    if($inforesultado["intento"]<=0){
+                        $intento=1;
+                        $iestatus=1;
+    
+                 }
+                 else if($inforesultado["intento"]>2)
+                 {
+                    //restear
+                    $validarvisto=mysqli_query($conn,
+                    "SELECT* FROM visto v
+                     INNER JOIN inscripcion i
+                     ON v.idAlumno=i.fkiIdUsuario
+                     WHERE v.idAlumno=" .$idusuario.
+                    " AND i.fkiIdeCurso=".$idcurso);
+    
+                   
+                    while ($visto = mysqli_fetch_array($validarvisto))
+                    { 
+                        $dataresultado = mysqli_query($conn,
+                        "UPDATE visto  SET estatus=0 
+                         WHERE idvisto=" .$visto["idVisto"]);
+                         if ($dataresultado== TRUE) {
+                     
+                            $dataresultad2 = mysqli_query($conn,
+                            "DELETE FROM  resuelto 
+                             WHERE idcurso=" .$idcurso.
+                            " AND idusuario=" .$idusuario.
+                            " AND idexamen=" .$idexamen ); 
+                                $iestatus=0;
+                          
+                              
+                             } else{
+                                echo '<script type="text/JavaScript"> location.reload(); </script>';
+                             }
+    
+                    }
+                    
+                 }
+                 else
+                 {//agrgar intentos
+                    $intento=$inforesultado["intento"]+1;
+                    $iestatus=1;
+                 }
+                }else{
                     $intento=1;
                     $iestatus=1;
-
-             }
-             else if($inforesultado["intento"]>=2)
-             {
-                //restear
-                $validarvisto=mysqli_query($conn,
-                "SELECT* FROM visto v
-                INNER JOIN inscripcion i
-                ON v.idAlumno=i.fkiIdUsuario
-                WHERE v.idAlumno=" .$idusuario.
-                " AND i.fkiIdeCurso=".$idcurso);
-
-               
-                while ($visto = mysqli_fetch_array($validarvisto))
-                { 
-                    $dataresultado = mysqli_query($conn,
-                    "UPDATE visto  SET estatus=0 
-                     WHERE idvisto=" .$visto["idVisto"]);
-                     if ($dataresultado== TRUE) {
-                 
-                        $dataresultad2 = mysqli_query($conn,
-                        "UPDATE  resuelto SET iEstatus=0 
-                         WHERE idcurso=" .$idcurso.
-                        " AND idusuario=" .$idusuario.
-                        " AND idexamen=" .$idexamen ); 
-                            $iestatus=0;
-                      
-                          
-                         } else{
-                           header("Location:../alumno/index.php");
-                         }
-
                 }
-                
-             }
-             else
-             {//agrgar intentos
-                $intento=$inforesultado["intento"]+1;
-                $iestatus=1;
-             }
+             
 
              $data.=", intento=".$intento;
              $data.=", iestatus=".$iestatus;
