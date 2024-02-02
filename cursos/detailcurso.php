7aa857b331abea7c015685044cfe4da20c5d077c
Dereckz <dereckz1304@gmail.com>
2024-02-02 09:25:30 -0600

    ayudame

--- cursos/detailcurso.php
+++ cursos/detailcurso.php
@@ -473,7 +473,20 @@
                 }
         }else{
                 $constancia="";
-                $examen="";
+                $examen='
+                    <tr>
+                    <td></td>
+                    <td></td>
+                        <td class=constancia>
+                            <a href="../cursos/evaluacion.php?idC='.$idcursoseleccionado.'" 
+                            id="texconstancia" target="_blank" 
+                            class="u-border-1 u-border-active-grey-70 u-border-black u-border-hover-grey-70 u-border-no-left u-border-no-right u-border-no-top u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-button-style u-custom-item u-none u-radius-0 u-text-active-palette-2-base u-text-body-color u-text-hover-palette-2-base u-top-left-radius-0 u-top-right-radius-0 u-btn-2">
+                            Realizar Examen
+                            <br>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr><td     colspan=3 class=celdasvacias></td></tr>';  
         }
         if($masDedos==3){
             $mensaje= '<tr><td></td><td></td><td class=notacurso>Nota: Este curso ya fue tomado anteriormente.</td></tr>';
