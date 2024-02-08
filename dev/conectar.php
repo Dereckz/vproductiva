731f9eacce99b85fbda4b005d19d11f380c8ab4e
Merino <cesar.merinopmc@gmail.com>
2024-02-08 11:22:45 -0600

    cambios 3 Merino 08022024

--- dev/conectar.php
+++ dev/conectar.php
@@ -1,12 +1,12 @@
 <?php
 
-
+/*
 $dbhost = "localhost";
 $dbuser = "valuaci4_administradorbd";
 $dbpass = "MK7GwJ.NiF{4";
 $dbname = "valuaci4_valuacionproductivabd";
 
- /*
+ */
  
  $dbhost = "localhost";
  $dbuser = "root";
@@ -17,6 +17,6 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn) 
 {
 	die("No hay conexión: ".mysqli_connect_error());
-}*/
+}
 
 ?>
