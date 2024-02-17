9c62883c337a8d98823146afa4c58f24e525aa4b
Dereckz <dereckz1304@gmail.com>
2024-02-17 02:33:39 -0600

    index contacto

--- contacto.php
+++ contacto.php
@@ -4,51 +4,76 @@
 <?php
 require('dev/conectar.php');
 date_default_timezone_set('America/Mexico_City');
- 
+echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
 $fechaActual = date("d-m-Y h:i:s");
-try{
+
     $nombre=($_POST["name"]);
     $apellidos=($_POST["apellido"]);
     $email=($_POST["email"]);
     $telefono=($_POST["numero"]);
-    
-    $queryC = mysqli_query($conn,"INSERT INTO `contacto`(`idcontacto`, `nombre`, `apellidos`, `correo`, `telefono`, `estatus`)
-    VALUES ('0','$nombre','$apellidos','$email','$telefono','1');");
-     if($queryC){
-        echo '<script>
-            Swal.fire({
-            title: "Gracias por escribirnos, en breve nos comunicamos con usted.",
-            icon: "success"
-        }).then(function() {
-            window.location = "index.html";
-        });
-           
-        </script>';
-        '<script language="javascript">alert("Gracias por escribirnos, en breve nos comunicamos con usted. ");</script>';
-        header('Location: index.html');          
+    if(!isset($nombre)||!isset($apellidos)||!isset($email)||!isset($telefono)){
+        echo'.';
+        /*  echo '<script>
+                Swal.fire({
+                title: "Debe rellenar todos los datos.",
+                icon: "error"
+            }).then(function() {
+                window.location = "../indexcontacto.html";
+            });
+            
+            </script>';  */
     }else{
-        echo '<script>
-            Swal.fire({
-            title: "Hubo un error, vuelve a intentarlo nuevamente",
-            icon: "error"
-        }).then(function() {
-            window.location = "../index.html";
-        });
-           
-        </script>'; 
-       // '<script language="javascript">alert("Hubo un error, vuelve a intentarlo nuevamente");</script>';
-        header('Location: index.html');          
+        $queryC = mysqli_query($conn,"INSERT INTO `contacto`(`idcontacto`, `nombre`, `apellidos`, `correo`, `telefono`, `estatus`)
+        VALUES ('0','$nombre','$apellidos','$email','$telefono','1');");
+         if($queryC){
+            echo'.';
+            echo '<script>
+                Swal.fire({
+                title: "Gracias por escribirnos, en breve nos comunicamos con usted.",
+                icon: "success"
+            }).then(function() {
+                window.location = "index.html";
+            });
+               
+            </script>';
+            '<script language="javascript">alert("Gracias por escribirnos, en breve nos comunicamos con usted. ");</script>';
+           //header('Location: index.html');          
+        }else{
+            $queryC = mysqli_query($conn,"INSERT INTO `contacto`(`idcontacto`, `nombre`, `apellidos`, `correo`, `telefono`, `estatus`)
+            VALUES ('0','$nombre','$apellidos','$email','$telefono','1');");
+             if($queryC){
+                echo'.';
+                echo '<script>
+                    Swal.fire({
+                    title: "Gracias por escribirnos, en breve nos comunicamos con usted.",
+                    icon: "success"
+                }).then(function() {
+                    window.location = "../indexcontacto.html";
+                });
+                   
+                </script>';
+                '<script language="javascript">alert("Gracias por escribirnos, en breve nos comunicamos con usted. ");</script>';
+                //header('Location: index.html');          
+            }else{
+                echo'.';
+                echo '<script>
+                    Swal.fire({
+                    title: "Hubo un error, vuelve a intentarlo nuevamente",
+                    icon: "error"
+                }).then(function() {
+                    window.location = "../indexcontacto.html";
+                });
+                   
+                </script>'; 
+               // '<script language="javascript">alert("Hubo un error, vuelve a intentarlo nuevamente");</script>';
+              //  header('Location: index.html');          
+            }
+        
+           // '<script language="javascript">alert("Hubo un error, vuelve a intentarlo nuevamente");</script>';
+           // header('Location: index.html');          
+        }
+    
     }
-}catch(ex){
-    echo '<script>
-    Swal.fire({
-    title: "Hubo un error, vuelve a intentarlo nuevamente",
-    icon: "error"
-}).then(function() {
-    window.location = "../index.html";
-});
-   
-</script>'; 
-}
+  
 
 ?>
--- cursos/view.php
+++ cursos/view.php
@@ -23,7 +23,7 @@ $ruta="../cursos/recurso/".$recurso;
 
 	video{
 		width: 100%;
-		height: 90%;
+		height: 100%;
 	}
 	video::-internal-media-controls-download-button {
 		display:none;
@@ -37,55 +37,80 @@ $ruta="../cursos/recurso/".$recurso;
 		width: calc(100% + 25px);
 	}
 	
-		
+
 </style>
 
 <!DOCTYPE html>
 <html>
     <head>
-	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
-
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+	<link rel="stylesheet" href="./style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/boostrap.bundle.min.js"></script>
 	</head>
 	<body>
 
 	
 
-		<?php if($formato=="pdf"){ 
+		<?php 
+		$iphone=strpos($_SERVER['HTTP_USER_AGENT'],'iphone');
+		$android=strpos($_SERVER['HTTP_USER_AGENT'],'Android');
+		$ipod=strpos($_SERVER['HTTP_USER_AGENT'],'Ipod');
+
+		if($formato=="pdf"){ 
 			
 			$archivoconextension= explode('/',$datarec['cRuta']);
 			$pdf2=$archivoconextension[3];
 
 			$file = $archivoconextension[2].'/'.$archivoconextension[3];
 			$filename = $archivoconextension[3];
+				if($iphone || $android || $ipod){
+						echo '<object data="mypdf.pdf" type="application/pdf" frameborder="0" width="100%" height="600px" style="padding: 20px;">
+						<embed src='.$file.' width="100%" height="100%" >';
+				}else{
+					header('Content-type: application/pdf');
+					header('Content-Disposition: inline; filename="'.$filename	.'"');
+					header('Content-Transfer-Encoding: binary');
+					header('Accept-Ranges: bytes');
+					@readfile($file);  
+				}
 			
-			
-		
-			 header('Content-type: application/pdf');
-			header('Content-Disposition: inline; filename="'.$filename	.'"');
-			header('Content-Transfer-Encoding: binary');
-			header('Accept-Ranges: bytes');
-			@readfile($file); 
 			
 			
 		?>
-	
+		
+		<!-- <embed src="<?php echo $file ?>#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" width="100%" height="600px" />
+	 -->
 		  <!-- <div id="pdf" style="border:0;">
 			<object data="mypdf.pdf" type="application/pdf" frameborder="0" width="100%" height="600px" style="padding: 20px;">
 			<embed src=<?php echo $file ?> width="100%" height="100%" >
 		</object>
 		</div>   -->
-		  <!-- <div class="container"> -->
-   			<!-- <iframe style="float:right;" src = "ViewerJS/#"<?php echo $file ?> width='100%' height='100%' allowfullscreen webkitallowfullscreen></iframe>	
-		    	</div>  -->
+		
 		 
 		<?PHP
 
-		}elseif ($formato=="mp4"){?>
+		}elseif ($formato=="mp4"){
+			if($iphone || $android || $ipod){
+				header("Location:".$datarec["cRuta"])
+
+			?>
+		<!-- cel-->
+		<?php
+		
+			}else{
+			?>
+		
 			<video id="player" playsinline controls controlsList="nodownload" autoplay >
-			<source src=<?PHP echo ($datarec["cRuta"]);?> type="video/mp4" />
-			</video>
+				<source src=<?PHP echo ($datarec["cRuta"]);?> type="video/mp4" />
+				</video>
 			
-		<?php } ?>
+			
+			
+		<?php }
+	} 
+	
+	?>
 </body>		
 	
 </html>
@@ -95,6 +120,11 @@ $ruta="../cursos/recurso/".$recurso;
 		if (elem.requestFullscreen) {
 		elem.requestFullscreen();
 		}
+
+
+
 	</script> 
+
+	
 
 
