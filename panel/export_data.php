bbab6f05dc2eae3f68788677f4c705fd1fde3189
Dereckz <dereckz1304@gmail.com>
2024-03-08 16:49:20 -0600

    exporte

--- panel/export_data.php
+++ panel/export_data.php
@@ -1,23 +1,26 @@
 <?php
-//$filename="Datos Alumnos";
-if(isset($_POST["export_data"])) {
-    $filename = "phpzag_data_export_".date('Ymd') .".xlsx";
-    header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header("Content-Disposition: attachment; filename=".$filename."");
-    header('Content-Transfer-Encoding: binary');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    $show_coloumn = false;
-    if(!empty($developer_records)) {
-    foreach($developer_records as $record) {
-    if(!$show_coloumn) {
-    // display field/column names in first row
-    echo implode("t", array_keys($record)) . "n";
-    $show_coloumn = true;
-    }
-    echo implode("t", array_values($record)) . "n";
-    }
-    }
-    exit;
-    }
+
+$sql_query = "SELECT * FROM empresa";
+$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+$developer_records = array();
+while( $rows = mysqli_fetch_assoc($resultset) ) {
+	$developer_records[] = $rows;
+}	
+if(isset($_POST["export_data"])) {	
+	$filename = "phpzag_data_export_".date('Ymd') . ".xls";			
+	header("Content-Type: application/vnd.ms-excel");
+	header("Content-Disposition: attachment; filename=\"$filename\"");	
+	$show_coloumn = false;
+	if(!empty($developer_records)) {
+	  foreach($developer_records as $record) {
+		if(!$show_coloumn) {
+		  // display field/column names in first row
+		  echo implode("\t", array_keys($record)) . "\n";
+		  $show_coloumn = true;
+		}
+		echo implode("\t", array_values($record)) . "\n";
+	  }
+	}
+	exit;  
+}
 ?>
